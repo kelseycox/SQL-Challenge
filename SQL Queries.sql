3f90c9675c44a8296eeb13435cd3586ceb67df8e
Kelsey Cox <kelseyjenecox@gmail.com>
2020-10-19 23:37:18 -0700

    code change

--- SQL Queries.sql
+++ SQL Queries.sql
@@ -21,7 +21,7 @@ INNER JOIN departments ON dept_emp.dept_no = departments.dept_no;
 --List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
 SELECT first_name, last_name FROM employees 
 WHERE first_name = 'Hercules' 
-AND last_name LIKE 'B%'
+AND last_name LIKE 'B%';
 
 --List all employees in the Sales department, including their employee number, last name, first name, and department name.
 SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name FROM dept_emp
